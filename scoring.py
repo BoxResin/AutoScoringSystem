199af7bec59d1d9d0dd72a3e2222eb0eef2c5b11
Minsuk Eom <boxresin@gmail.com>
2017-03-19 22:39:59 +0900

    채점 기능 완성

---
+++ scoring.py
@@ -0,0 +1,73 @@
+# (실행할 수 있는 모듈)
+# 첫 번째 인수로 지정된 디렉토리의 소스코드를 채점한다.
+# ex) python scoring.py something/ <- something 디렉토리의 소스코드를 채점
+
+import os
+import sys
+import subprocess
+import glob
+from settings import *
+
+if len(sys.argv) < 2:
+    print('채점할 디렉토리를 첫 번째 인수로 지정해주세요.')
+    sys.exit()
+
+TARGET_PATH = sys.argv[1]
+
+
+def wrong_answer(number, msg):
+    print('%d번 문제 오답' % number)
+    print('이유: ' + msg + '\n\n')
+
+
+def check_problem(number):
+    print('================================')
+    code_path = '%s/%d.c' % (TARGET_PATH, number)
+
+    # 찌꺼기 삭제
+    if os.path.exists('a.exe'):
+        os.remove('a.exe')
+
+    # i번째 소스코드가 있나 확인
+    if not os.path.exists(code_path):
+        raise Exception('%d.c 소스코드가 없습니다.' % number)
+
+    # i번째 소스코드 컴파일
+    subprocess.run('gcc %s' % code_path)
+
+    # 컴파일 실패했는지 확인
+    if not os.path.exists('a.exe'):
+        raise Exception('컴파일 실패')
+
+    # 컴파일된 프로그램에 input 넣어보고 정답 output과 대조해보기
+    inputs = glob.glob('answer/%d_input*.txt' % number)
+    outputs = glob.glob('answer/%d_output*.txt' % number)
+
+    for i in range(len(inputs)):
+        prob_input = open(inputs[i], 'r')
+        prob_output = open(outputs[i], 'r')
+
+        output, err = subprocess.Popen('a.exe', stdin=prob_input, stdout=subprocess.PIPE).communicate()
+        program_output = output.decode().replace('\r\n', '\n').strip()
+        answer_str = prob_output.read()
+
+        prob_input.close()
+        prob_output.close()
+
+        if program_output == answer_str:
+            print('테스트 케이스 %d 통과' % (i + 1))
+        else:
+            raise Exception('테스트 케이스 %d 오답' % (i + 1))
+
+
+    print('%d번 문제 정답\n\n' % number)
+
+
+# 채점 시작
+for i in range(1, PROBLEM_COUNT + 1):
+    # i번째 문제 채점
+    try:
+        check_problem(i)
+    except Exception as e:
+        wrong_answer(i, str(e))
+
---
+++ settings.py
@@ -0,0 +1,1 @@
+PROBLEM_COUNT = 1  # 문제 개수
