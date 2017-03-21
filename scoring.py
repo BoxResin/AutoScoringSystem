ca0d298deacce2ccce85f69bc83217634c18b60d
Minsuk Eom <boxresin@gmail.com>
2017-03-22 01:23:17 +0900

    테스트 케이스 일부만 통과하면 부분점수를 주고, 총점을 출력하도록 함.

--- scoring.py
+++ scoring.py
@@ -20,6 +20,7 @@ def wrong_answer(number, msg):
     print('이유: ' + msg + '\n\n')
 
 
+# number번 문제를 채점하고 점수를 반환함.
 def check_problem(number):
     print('================================')
     code_path = '%s/%d.c' % (TARGET_PATH, number)
@@ -43,9 +44,10 @@ def check_problem(number):
     inputs = glob.glob('answer/%d_input*.txt' % number)
     outputs = glob.glob('answer/%d_output*.txt' % number)
 
-    ok = True
+    score = 0.0
+    test_count = len(inputs)
 
-    for i in range(len(inputs)):
+    for i in range(test_count):
         prob_input = open(inputs[i], 'r')
         prob_output = open(outputs[i], 'r')
 
@@ -60,31 +62,36 @@ def check_problem(number):
 
         if program_output == answer_str:
             print('테스트 케이스 %d 통과' % (i + 1))
+            score += PROBLEM_SCORE[number] * 1.0 / test_count
         else:
-            ok = False
             print('테스트 케이스 %d 오답\n' % (i + 1) +
                   '[입력]\n%s\n\n[정답]\n%s\n\n[프로그램의 출력값]\n%s\n\n' % (input_str, answer_str, program_output))
 
-    if not ok:
-        raise Exception('테스트 케이스 일부 오답')
-
-    print('%d번 문제 정답\n\n' % number)
+    return score
 
 
-RESULT = [None] * (PROBLEM_COUNT + 1)
+check_result = [None] * (PROBLEM_COUNT + 1)
+total_score = 0.0
 
 # 채점 시작
 for i in range(1, PROBLEM_COUNT + 1):
     # i번째 문제 채점
     try:
-        check_problem(i)
-        RESULT[i] = 'O'
+        score = check_problem(i)
+        if score == PROBLEM_SCORE[i]:  # 모든 테스트 케이스를 통과했으면
+            print('%d번 문제 정답\n\n' % i)
+            check_result[i] = 'O'
+        else:  # 테스트 케이스를 하나라도 통과하지 못했으면
+            print('%d번 문제 일부 오답\n\n' % i)
+            check_result[i] = '?'
+        total_score += score
     except Exception as e:
         wrong_answer(i, str(e))
-        RESULT[i] = 'X'
+        check_result[i] = 'X'
 
 # 결과를 요약하여 출력
 print("========== 요약 ==========")
-print("O: 정답, X: 오답")
+print("O: 정답, X: 오답, ?: 일부만 정답")
 for i in range(1, PROBLEM_COUNT + 1):
-    print('%d. [%s]' % (i, RESULT[i]), end='  /  ')
+    print('%d. [%s]' % (i, check_result[i]), end='  /  ')
+print("\n총점: " + str(total_score))
--- settings.py
+++ settings.py
@@ -1,2 +1,7 @@
 PROBLEM_COUNT = 1  # 문제 개수
 COMPILER = 'c99'  # 컴파일러 버전
+
+# 문제당 배점 (키: 문제번호, 값: 배점)
+PROBLEM_SCORE = {
+    1: 100,
+}
